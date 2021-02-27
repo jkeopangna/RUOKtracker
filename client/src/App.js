13e1e26b293db49b7f2042c6e46008282c143e5a
Jules <juliannewood2413@gmail.com>
2021-02-27 13:55:42 -0700

    updated with react-bootstrap, added form component and login page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './components/pages/Home';
 import UserPage from './components/pages/UserPage';
 import Clinics from './components/pages/Clinics';
+import LogIn from './components/pages/LogIn';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
        <Route path="/" exact component ={Home} />
        <Route path="/userpage" exact component ={UserPage} />
        <Route path="/clinics" exact component ={Clinics} />
+       <Route path="/login" exact component={LogIn} />
      </Switch>
     </Router>
     </>
