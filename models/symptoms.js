c6d675ca2e9d869d9eba577f9b583b6f577eb77b
Jon Keopangna <jkeopangna@gmail.com>
2021-03-04 20:35:19 -0700

    Added/modified models, controllers, routes

--- models/symptoms.js
+++ models/symptoms.js
@@ -8,7 +8,8 @@ const symptomSchema = new Schema ({
     },
     symptoms: { type: Array, required: true },
     Nausea: {type: Number, required: true },
-    Nausea: {type: Number, required: true },
+    BodyPain: {type: Number, required: true },
+    Fatigue: {type: Number, required: true}, 
     date: { type: Date, default: Date.now }
 });
 
