9a30851c6a202aa9fb6f265fd65d34121e72290d
Kai Curtis <kai6curtis@gmail.com>
2021-03-09 21:22:29 -0700

    map and geolocator

--- client/src/components/pages/Clinics.js
+++ client/src/components/pages/Clinics.js
@@ -1,20 +1,18 @@
-import React from 'react'
-import '../../App.css';
-import Footer from '../Footer';
+import React from "react";
+import "../../App.css";
+import Footer from "../Footer";
+import Map from "../Map";
 
-
-export default function Clinics () {
-    return (
-        <>
-    
-        <div className="clinics">
-            <h1>FIND A CLINIC PAGE GOES HERE</h1>
-            
-            
-        </div>
-        
-    <Footer />
+export default function Clinics() {
+  return (
+    <>
+      <div className="clinics">
+        <h1>FIND A CLINIC</h1>
+      </div>
+      <div className="map-container">
+        <Map />
+      </div>
+      <Footer />
     </>
-
-        )
-    }
+  );
+}
